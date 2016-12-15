475c02b9bf58b8fac0c22aaf49af713da89031d9
Mahesh Saha <sahamahesh20@gmail.com>
2016-12-15 05:48:34 -0500

    bubbleSort works? I don't know if it is the right processing time yet.

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -25,21 +25,47 @@ public class Sorts{
 	    index++;
 	}
     }
-    
+    /**Insertion sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
     public static void insertionSort(int[]data){
 	for(int index = 1; index < data.length; index++){
-	    for(int j = 0; j < data.length-1; j++)
-	    if (data[index] < data[j]){
-		int temp = data[j];
-		data[j] = data[index];
-		data[index] = temp;
+	    for(int j = 0; j < data.length-1; j++){
+		if (data[index] < data[j]){
+		    int temp = data[j];
+		    data[j] = data[index];
+		    data[index] = temp;
+		}
 	    }
 	}
     }
-		   
+    /**Bubble sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void bubbleSort(int[]data){
+    	for(int index = 0; index < data.length - 1; index++){
+	    if(data[index]>data[index+1]){
+		int temp = data[index];
+		data[index] = data[index+1];
+		data[index+1] = temp;
+	    }
+      	}
+	int i = 1;
+	int counter = 0;
+	while(i < data.length){
+	    if (data[i-1]>data[i])
+	        bubbleSort(data);
+	    i++;
+	}
+    }
+
+    
+  
     public static void main(String[]arg){
     	int []data = {3,1,2,3,6,54,7,4,2,8};
-	selectionSort(data);
+        bubbleSort(data);
     	for (int i = 0; i < data.length; i++){
     	    System.out.println(data[i]);
     	}
